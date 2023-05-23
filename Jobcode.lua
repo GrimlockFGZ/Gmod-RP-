1b910c2cab0840af514f91768c9b830ef9cc1968
BBGDylan <31493391+BBGDylan@users.noreply.github.com>
2023-05-23 17:48:22 +0100

    suck your mum grim

--- Jobcode.lua
+++ Jobcode.lua
@@ -1,1 +1,27 @@
+--[[
+    Teammitglied im Dienst Job
+    Generated using: DarkRP | Job Generator
+    https://yourdevtools.com/gmod/darkrp-job
+--]]
 
+TEAM_Staff = DarkRP.createJob("Staff on Duty", {
+    color = Color(255, 0, 0),
+    model = "models/player/anon/anon.mdl",
+    description = [[
+        Handle tickets and reports
+    ]],
+    weapons = {
+        " weapon_physgun",
+        "weapon_physcannon",
+        "gmod_camera",
+        "weapon_empty_hands",
+        "gas_log_scanner"
+    },
+    command = "SOD",
+    max = 0,
+    salary = 2000,
+    admin = 1,
+    vote = false,
+    hasLicense = false,
+    canDemote = false,
+})
